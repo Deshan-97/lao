a15d6d3ef242524247f4dcd760ec1394bdf9fe63
Deshan-97 <deshanweththasingha@gmail.com>
2025-07-27 19:33:40 +0530

    first commit

---
+++ public/script.js
@@ -0,0 +1,4 @@
+// This file is kept for any future shared JavaScript functionality
+// All lottery-specific code is now in the respective HTML files
+
+console.log('Galaxy Lottery system loaded'); 
